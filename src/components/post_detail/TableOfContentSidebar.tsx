7e1e8b8ff4ddb258b817c87fe4fea9ca5caa5bbd
osohyun0224 <53892427+osohyun0224@users.noreply.github.com>
2025-02-14 18:40:41 +0900

    🐛 fix :: 레이아웃 재구성

--- src/components/post_detail/TableOfContentSidebar.tsx
+++ src/components/post_detail/TableOfContentSidebar.tsx
@@ -13,7 +13,7 @@ interface Props {
 }
 
 const TableOfContent = ({ toc }: Props) => {
-  const activeIdList = useHeadingsObserver('h2, h3');
+  const activeIdList = useHeadingsObserver('h1, h2');
 
   return (
     <aside className='not-prose absolute -top-[200px] left-full -mb-[100px] hidden h-[calc(100%+150px)] xl:block '>
